279f1dff853fead64293048f269caab155d17853
pete <mcgarrigle@github.com>
2026-02-23 19:00:05 +0000

    setup install mini.deps

--- setup.sh
+++ setup.sh
@@ -12,4 +12,13 @@ rm -rf "${DIR}/${PACKAGE}" "${DIR}/nvim"
 curl -L https://github.com/neovim/neovim/releases/latest/download/${PACKAGE}.tar.gz|tar xz
 ln -sf "${DIR}/${PACKAGE}" "${DIR}/nvim"
 
+export PATH="${HOME}/.local/share/nvim/bin":$PATH
+
+mkdir -p ~/.local/share/nvim/site/pack/deps/start/
+cd ~/.local/share/nvim/site/pack/deps/start/
+
+git clone --filter=blob:none https://github.com/nvim-mini/mini.nvim
+
+nvim --headless -c 'helptags ALL' -c 'quit'
+
 ls -l $DIR
